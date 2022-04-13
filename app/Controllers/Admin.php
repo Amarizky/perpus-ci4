c92aa35fd6ed1f595487e3ef6e2cdcb0817a4ed7
Amarizky Yoga Pratama <amarizky02@gmail.com>
2022-04-13 13:00:56 +0700

    refactor: removed unnecessary function on Admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -13,35 +13,11 @@ class Admin extends BaseController
         $page = $this->request->getGet('page') ?? 1;
         $bookList = $bookModel->getAllBooks();
 
-        // dd([
-        //     'pageTitle' => 'Daftar Buku',
-        //     'bookList'  => $bookList->paginate(20),
-        //     'pager'     => $bookList->pager,
-        //     'page'      => $page,
-        // ]);
-
         return view('admin/book_list', [
             'pageTitle' => 'Daftar Buku',
             'bookList'  => $bookList->paginate(20),
             'pager'     => $bookList->pager,
             'page'      => $page,
-        ]);
-    }
-    public function test()
-    {
-        $bookModel = new BookModel();
-        $page = $this->request->getGet('page') ?? 1;
-
-        dd([
-            'pageTitle' => 'Daftar Buku Test',
-            'bookList'  => $bookModel->getAllBooksTest()->paginate(20),
-            'pager'     => $bookModel->pager
-        ]);
-
-        return view('admin/book_list', [
-            'pageTitle' => 'Daftar Buku Test',
-            'bookList'  => $bookModel->getAllBooksTest(),
-            'pager'     => $bookModel->pager
         ]);
     }
 }
