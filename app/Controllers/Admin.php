cfc6e6fcc5d5fe291a8a019d34d35895a044c200
Amarizky Yoga Pratama <amarizky02@gmail.com>
2022-04-13 14:17:43 +0700

    feat: edit book

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -4,20 +4,52 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\BookModel;
+use App\Models\CategoryModel;
 
 class Admin extends BaseController
 {
     public function index()
     {
-        $bookModel = new BookModel();
-        $page = $this->request->getGet('page') ?? 1;
-        $bookList = $bookModel->getAllBooks();
+        $bookModel     = new BookModel();
+        $categoryModel = new CategoryModel();
+
+        $page          = $this->request->getGet('page') ?? 1;
+        $bookList      = $bookModel->getAllBooks();
 
         return view('admin/book_list', [
-            'pageTitle' => 'Daftar Buku',
-            'bookList'  => $bookList->paginate(20),
-            'pager'     => $bookList->pager,
-            'page'      => $page,
+            'pageTitle'   => 'Daftar Buku',
+            'bookList'    => $bookList->paginate(20),
+            'pager'       => $bookList->pager,
+            'page'        => $page,
+            'categories'  => $categoryModel->findAll(),
         ]);
+    }
+
+    public function book_edit()
+    {
+        $bookModel = new BookModel();
+        $id        = $this->request->getPost('book-id');
+        $data      = [
+            'name'        => $this->request->getPost('name'),
+            'category_id' => $this->request->getPost('category-id'),
+            'author'      => $this->request->getPost('author'),
+            'year'        => $this->request->getPost('year'),
+        ];
+
+        $bookModel->update($id, $data);
+
+        return redirect()->to('/admin');
+    }
+
+    public function get_book_data()
+    {
+        if (!$this->request->isAJAX()) return;
+
+        $bookModel = new BookModel();
+        $id        = $this->request->getPost('book_id');
+        $res       = $bookModel->find($id);
+        $res->csrf = csrf_hash();
+
+        return json_encode($res);
     }
 }
--- app/Views/admin/book_list.php
+++ app/Views/admin/book_list.php
@@ -10,7 +10,12 @@ use CodeIgniter\I18n\Time;
 ?>
 
 <div class="container-fluid">
-    <div class="row my-4">
+    <div class="row mt-4">
+        <div class="col">
+            <?= $pager->links('default', 'bootstrap'); ?>
+        </div>
+    </div>
+    <div class="row mb-2">
         <div class="col text-end">
             <a href="#" class="btn btn-primary">Tambah Buku Baru</a>
         </div>
@@ -20,15 +25,15 @@ use CodeIgniter\I18n\Time;
             <table class="table table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th scope="col" style="width: 50px;" class="text-center">#</th>
+                        <th scope="col" style="width: 52px;" class="text-center">#</th>
                         <th scope="col" style="width: 340px;">Nama Buku</th>
                         <th scope="col" style="width: 120px;">Kategori</th>
                         <th scope="col" style="width: 160px;">Penulis</th>
                         <th scope="col" style="width: 62px;" class="text-center">Tahun Terbit</th>
                         <th scope="col" class="text-center">Dipinjam?</th>
-                        <th scope="col">Tanggal Pinjam</th>
-                        <th scope="col">Tanggal Kembali</th>
-                        <th scope="col" style="width: 140px;" class="text-center">Aksi</th>
+                        <th scope="col" style="width: 100px;">Tanggal Pinjam</th>
+                        <th scope="col" style="width: 100px;">Tanggal Kembali</th>
+                        <th scope="col" style="width: 150px;" class="text-center">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,9 +46,9 @@ use CodeIgniter\I18n\Time;
                             <td class="align-middle"><?= $b->author; ?></td>
                             <td class="align-middle text-center"><?= $b->year; ?></td>
                             <td class="align-middle text-center"><?= str_replace('-', '<br>', $b->loaned_to ?? 'Tidak'); ?></td>
-                            <td class="align-middle"><?= $b->loaned_at ? Time::createFromTimestamp($b->loaned_at)->toLocalizedString('HH:m d-MM-Y') : '-'; ?></td>
+                            <td class="align-middle"><?= $b->loaned_at ? 'Jam ' . str_replace(' ', "<br>", Time::createFromTimestamp($b->loaned_at)->toLocalizedString('HH:m d-MM-Y')) : '-'; ?></td>
                             <td class="align-middle"><?= $b->returns_in ? Time::createFromTimestamp($b->returns_in)->toLocalizedString('d-MM-Y') : '-'; ?></td>
-                            <td class="align-middle">
+                            <td class="align-middle text-center">
                                 <a href="#" data-book-id="<?= $b->id; ?>" style="width: 58px;" class="btn btn-sm btn-success btn-edit">Edit</a>
                                 <a href="#" data-book-id="<?= $b->id; ?>" style="width: 58px;" class="btn btn-sm btn-warning btn-delete">Hapus</a>
                             </td>
@@ -60,20 +65,75 @@ use CodeIgniter\I18n\Time;
     </div>
 </div>
 
-<form action="#" method="post" class="modal" tabindex="-1">
-    <?= csrf_field(); ?>
+<form id="book-edit" action="<?= base_url('admin/book_edit') ?>" method="post" class="modal" data-bs-backdrop="static" tabindex="-1">
+    <input type="hidden" name="csrf_test_name" id="csrf" value="<?= csrf_hash(); ?>">
+    <input type="hidden" name="book-id" id="book-id">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <div class="modal-title">Edit Buku</div>
+                <h5 class="modal-title">Edit Buku</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body"></div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama Buku</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Masukkan nama buku">
+                </div>
+                <div class="mb-3">
+                    <label for="category-id" class="form-label">Kategori</label>
+                    <select class="form-select" id="category-id" name="category-id">
+                        <option value="null" disabled selected>Pilih salah satu</option>
+                        <?php foreach ($categories as $category) : ?>
+                            <option value="<?= $category->id; ?>"><?= $category->name; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="author" class="form-label">Penulis</label>
+                    <input type="text" class="form-control" id="author" name="author" placeholder="Masukkan nama penulis">
+                </div>
+                <div class="mb-3">
+                    <label for="year" class="form-label">Tahun</label>
+                    <input type="number" class="form-control" id="year" name="year" placeholder="Masukkan tahun">
+                </div>
+            </div>
             <div class="modal-footer">
-                <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                <button type="button" class="btn btn-primary">Simpan</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
         </div>
     </div>
 </form>
+
+<script>
+    var csrf = '<?= csrf_hash(); ?>';
+
+    $('.btn-edit').click(function() {
+        $.ajax({
+            url: '<?= base_url('/admin/get_book_data'); ?>',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            type: 'post',
+            data: {
+                book_id: $(this).data('book-id'),
+                <?= csrf_token(); ?>: csrf,
+            },
+            success: function(data) {
+                data = JSON.parse(data);
+                csrf = data.csrf;
+                $('#csrf').val(data.csrf);
+
+                $('#book-id').val(data.id);
+                $('#name').val(data.name);
+                $('#category-id').val(data.category_id).change();
+                $('#author').val(data.author);
+                $('#year').val(data.year);
+            },
+        });
+
+        $('#book-edit').modal('show');
+    })
+</script>
 
 <?= $this->endSection('content'); ?>
