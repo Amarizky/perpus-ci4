630c6fa7bc03526bf280f7d41ab610fa4feddca4
Amarizky Yoga Pratama <amarizky02@gmail.com>
2022-04-19 00:57:14 +0700

    feat: navbar for admin

--- app/Views/navbar-admin.php
+++ app/Views/navbar-admin.php
@@ -1,3 +1,5 @@
+<?php $seg = current_url(true)->getSegments(); ?>
+<!-- TODO: Rename navbar-admin to admin/navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-admin" aria-controls="navbar-admin" aria-expanded="false" aria-label="Buka bilah navigasi">
@@ -6,13 +8,13 @@
         <div class="collapse navbar-collapse" id="navbar-admin">
             <ul class="navbar-nav my-2 my-lg-0">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active">Buku</a>
+                    <a href="/admin/books" class="nav-link <?= $seg[1] == 'books' ? 'active' : ''; ?>">Buku</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Kategori</a>
+                    <a href="/admin/categories" class="nav-link <?= $seg[1] == 'categories' ? 'active' : ''; ?>">Kategori</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">Pengunjung</a>
+                    <a href="/admin/visitors" class="nav-link <?= $seg[1] == 'visitors' ? 'active' : ''; ?>">Pengunjung</a>
                 </li>
             </ul>
             <ul class="navbar-nav ms-auto">
